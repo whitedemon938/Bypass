140b913931ac1f92b51916195b0a19e1c2f943af
White Demon <133562822+whitedemon938@users.noreply.github.com>
2025-05-17 21:45:59 +0530

    Update alive.py

--- alive.py
+++ alive.py
@@ -1,4 +1,3 @@
-
 from asyncio import sleep as asleep, run
 from aiohttp import ClientSession
 import logging
@@ -15,18 +14,17 @@ logging.basicConfig(
 )
 
 async def check_server():
-    if PORT is not None:
-        async with ClientSession() as session:
-            while True:
-                try:
-                    res = await session.get(BASE_URL)
-                    if res.status == 200:
-                        logging.info(f"Server's Up. Status Code: {res.status}")
-                    else:
-                        logging.warning(f"Server Returned Status Code: {res.status}")
-                except Exception as e:
-                    logging.error(f"alive.py Exception: {e}")
-                await asleep(600)
+    async with ClientSession() as session:
+        while True:
+            try:
+                res = await session.get(BASE_URL)
+                if res.status == 200:
+                    logging.info(f"Server's Up. Status Code: {res.status}")
+                else:
+                    logging.warning(f"Server Returned Status Code: {res.status}")
+            except Exception as e:
+                logging.error(f"alive.py Exception: {e}")
+            await asleep(600)
 
 if __name__ == "__main__":
     run(check_server())
