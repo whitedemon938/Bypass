7fb518e6b12d698da44b5f7c32bee88fa020e7e3
White Demon <133562822+whitedemon938@users.noreply.github.com>
2025-05-17 20:27:43 +0530

    Update alive.py

--- alive.py
+++ alive.py
@@ -1,15 +1,20 @@
-from time import sleep
-from requests import get as rget
+from asyncio import  sleep as asleep
+from aiohttp import ClientSession
 from os import environ
 from logging import error as logerror
 from Bypass import BASE_URL, PORT
 
 if PORT is not None:
-    while 1:
-        try:
-            rget(BASE_URL).status_code
-            sleep(600)
-        except Exception as e:
-            logerror(f"alive.py: {e}")
-            sleep(600)
-            continue
+    async with ClientSession() as session:
+        while True:
+            try:
+                res = await session.get(BASE_URL)
+                if res.status == 200:
+                    print(f"Server's Up. Status Code: {res.status}")
+                    await asleep(600)
+                else:
+                    print(f"Server Returned Status Code: {res.status}")
+            except Exception as e:
+                logerror(f"alive.py: {e}")
+                await asleep(600)
+                continue
